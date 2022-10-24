7d7c9e3eebb194d1838cd5c98c569e2bcf860fb9
VinzentPilz <81297495+VinzentPilz@users.noreply.github.com>
2022-10-24 19:25:29 +0200

    first profile styling

--- pages/home.js
+++ pages/home.js
@@ -29,7 +29,7 @@ export default function Home() {
 
           <div id="userContainer">
             <LogoutButton />
-            <p id="username" className="userData">
+            <p id="username">
               Username
             </p>
             <div id="userImageContainer">
