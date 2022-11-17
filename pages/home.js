e042ad8966921a00e0dbfc6eaef74fa4fae7cdc4
VinzentPilz <81297495+VinzentPilz@users.noreply.github.com>
2022-11-17 16:04:11 +0100

    more table styling

--- pages/home.js
+++ pages/home.js
@@ -29,9 +29,19 @@ export default function Home() {
           <>
             <div id="buttonContainer">
               <hr></hr>
-              <div className={styles.tableContainer}>
+              <div className={styles.tableContainer} id="tableContainerLeft">
               <table className={styles.scoreTable}>
-                <caption>Normale Mode</caption>
+                
+                <tbody>
+                <tr>
+                    <td colSpan={"3"}>
+                      <select className={styles.tableSelect}>
+                        <option>Best Player First Mode</option>
+                        <option>Best Player Second Mode</option>
+                        <option>Best Player Third Mode</option>
+                      </select>
+                    </td>
+                  </tr>
                 <tr>
                   <th>Place</th>
                   <th>Player</th>
@@ -62,6 +72,7 @@ export default function Home() {
                   <td>Suq Maddiq</td>
                   <td>90054</td>
                 </tr>
+                </tbody>
               </table>
               </div>
 
@@ -73,39 +84,44 @@ export default function Home() {
                 </button>
               </Link>
 
-              <div className={styles.tableContainer}>
+              <div className={styles.tableContainer} id="tableContainerRight">
               <table className={styles.scoreTable}>
-                <caption>Normale Mode</caption>
+                
+                <tbody>
+                  <tr>
+                    <td id="tableHeadRight" colSpan={"3"}>Most played games</td>
+                  </tr>
                 <tr>
                   <th>Place</th>
                   <th>Player</th>
-                  <th>Score</th>
+                  <th>Games</th>
                 </tr>
                 <tr>
                   <td>1st</td>
                   <td>Hugh G. Rection</td>
-                  <td>20010911</td>
+                  <td>200</td>
                 </tr>
                 <tr>
                   <td>2nd</td>
                   <td>Ryan Stekken</td>
-                  <td>4206988</td>
+                  <td>42</td>
                 </tr>
                 <tr>
                   <td>3rd</td>
                   <td>Ben Dover</td>
-                  <td>573701</td>
+                  <td>41</td>
                 </tr>
                 <tr>
                   <td>4th</td>
                   <td>Hugh Jass</td>
-                  <td>125561</td>
+                  <td>12</td>
                 </tr>
                 <tr>
                   <td>5th</td>
                   <td>Suq Maddiq</td>
-                  <td>90054</td>
+                  <td>7</td>
                 </tr>
+                </tbody>
               </table>
               </div>
 
