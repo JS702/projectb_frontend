62d59ab5a4dff04ddcb28f5e2ef073310c84303d
Jon Steinkamp <steinkamp@moovin.de>
2022-12-09 19:21:05 +0100

    update profile

--- pages/profile.js
+++ pages/profile.js
@@ -4,6 +4,7 @@ import { useEffect, useRef, useState } from "react";
 import LoadingIndicator from "../components/loading-indicator";
 import Image from "next/image";
 import DefaultLayout from "../layouts/default-layout";
+import { useForm } from "react-hook-form";
 
 function Profile() {
     const [ user, setUser ] = useState();
@@ -11,6 +12,15 @@ function Profile() {
     const [ profileData, setProfileData ] = useState();
 
     const [ picture, setPicture ] = useState( { path: "/pepe.jpg" } );
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors }
+    } = useForm();
+
+    const keys = [ "Casual Score: ", "Roundtime Score: ", "Totaltime Score: ", "Casual Games Played: ", "Roundtime Games Played: ",
+        "Totaltime Games Played: " ];
 
     const inputRef = useRef();
 
@@ -34,6 +44,14 @@ function Profile() {
         inputRef.current.click();
     };
 
+    const onSubmit = async ( data ) => {
+        console.log( data );
+        try {
+            const response = await axiosInstance.post( "/user/update/" + user.id, data );
+            setUser( response.data );
+        } catch {
+        }
+    };
 
     const uploadPicture = async ( data ) => {
         const file = data.target.files[ 0 ];
@@ -79,21 +97,81 @@ function Profile() {
 
                 <div id="profileBodyContainer">
                     <hr/>
-                    <div id="descriptionContainer">{ user.description }</div>
+                    <form onSubmit={ handleSubmit( onSubmit ) }>
+                        <div className={ styles.inputContainer }>
+                            <label>Username</label>
+                            <input
+                                    className={ styles.input }
+                                    type={ "text" }
+                                    defaultValue={ user.username }
+                                    { ...register( "username", {
+                                        required: {
+                                            value: true,
+                                            message: "Username darf nicht leer sein!"
+                                        },
+                                        minLength: {
+                                            value: 3,
+                                            message:
+                                                    "Username muss mindestens 3 Buchstaben lang sein!"
+                                        }
+                                    } ) }
+                            />
+                            { errors.username && <p>{ errors.username.message }</p> }
+                        </div>
+
+                        <hr></hr>
+
+                        <div className={ styles.inputContainer }>
+                            <label>Email</label>
+                            <input
+                                    className={ styles.input }
+                                    type={ "text" }
+                                    defaultValue={ user.email }
+                                    { ...register( "email", {
+                                        required: {
+                                            value: true,
+                                            message: "Die Email darf nicht leer sein!"
+                                        },
+                                        pattern: {
+                                            value: /^\S+@\S+$/i,
+                                            message: "Die Email Adresse ist nicht valide"
+                                        }
+                                    } ) }
+                            />
+                            { errors.email && <p>{ errors.email.message }</p> }
+                        </div>
+
+                        <hr></hr>
+
+                        <div className={ styles.inputContainer }>
+                            <label>Beschreibung</label>
+                            <textarea
+                                    id="description"
+                                    className={ styles.input }
+                                    rows={ 4 }
+                                    defaultValue={ user.description }
+                                    { ...register( "description" ) }
+                            />
+                        </div>
+
+                        <hr></hr>
+
+                        <button id="buttonRegister" type="submit">
+                            Register
+                        </button>
+                    </form>
+
+
                     <div id="listContainer">
-                        { Object.keys( profileData ).map( ( key, idx ) => {
+                        { Object.entries( profileData ).map( ( [ key, value ], idx ) => {
+                            if ( !value ) {
+                                value = "No Data";
+                            }
                             return (
-                                    <li className={ styles.profileList } key={ idx }>{ key }</li>
+                                    <li className={ styles.profileList } key={ idx }>{ keys[ idx ] + value }</li>
                             );
                         } ) }
                         <br/>
-                    </div>
-                    <div id="listValueContainer">
-                        { Object.values( profileData ).map( ( value, idx ) => {
-                            return (
-                                    <li className={ styles.profileList } key={ idx }>{ value }</li>
-                            );
-                        } ) }
                     </div>
                 </div>
             </>
